bf09bab47bf60df91a6e439b903dd424e25bb07c
Ebuka <ebukaobiora10@gmail.com>
2023-06-02 18:24:53 +0100

    db-connect

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config()
 
 mongoose.connect(process.env.MONGO_URI
 , {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
