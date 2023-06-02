e4fa1fcba551e0fe2e13aec95f71b39e2bbc6da0
Ebuka <ebukaobiora10@gmail.com>
2023-06-02 18:18:59 +0100

    Mongo URI safety

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://dea-api:PassnET@cluster0.wq63ulc.mongodb.net/"
+mongoose.connect(process.env.MONGO_URI
 , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
