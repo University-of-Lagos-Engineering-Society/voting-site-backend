78d2b163acde71a8c3e04de6dfa0ed384c8fca41
olamide.aboyeji <olamide.aboyeji@konga.com>
2024-07-25 01:25:06 +0100

    category update

--- models/Category.js
+++ models/Category.js
@@ -4,10 +4,11 @@ const Schema = mongoose.Schema;
 
 const categorySchema = new Schema({
   _id: { type: Number },
-  name: { type: String, required: true, unique: true },
-  type: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  type: { type: String, required: true },
 }, { _id: false });
 
+categorySchema.index({ name: 1, type: 1 }, { unique: true });
 categorySchema.plugin(AutoIncrement);
 
 const Category = mongoose.model('Category', categorySchema);
@@ -16,7 +17,7 @@ const getCategories = async function() {
   const categories = await Category.find();
   const result = {};
   for(cat of categories) {
-    result[cat._id] = [cat.name, cat.type, cat.id];
+    result[cat._id] = [cat.name, cat.type, cat._id];
   }
   return result;
 };
