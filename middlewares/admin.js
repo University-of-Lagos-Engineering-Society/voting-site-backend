9c2cbccc6467b32881fe18095647fcf4f4727ef7
olamide.aboyeji <olamide.aboyeji@konga.com>
2024-08-02 20:36:39 +0100

    use app secret for non result routes

--- middlewares/admin.js
+++ middlewares/admin.js
@@ -1,7 +1,7 @@
 
 const isAdmin = (req, res, next) => {
     const secret = req.query.adminSecret;
-    if(!secret || secret !== process.env.ADMIN_SECRET) return res.status(401).json({ error: 'Not authorized to perform this action'});
+    if(!secret || secret !== process.env.APP_SECRET) return res.status(401).json({ error: 'Not authorized to perform this action'});
 
     return next();
 }
